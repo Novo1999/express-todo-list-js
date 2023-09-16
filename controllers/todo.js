5e74bc4c3f233b72ac06cb31063ab77f0fc08f0d
Novodip Mondal <neptune.navo@gmail.com>
2023-09-16 11:46:31 +0600

    some update

--- controllers/todo.js
+++ controllers/todo.js
@@ -6,67 +6,54 @@ const asyncWrapper = require("../middleware/async");
 const TODO = require("../models/Todo");
 
 const createTodo = asyncWrapper(async (req, res) => {
-  try {
-    const newTodo = await TODO.create(req.body);
-    return res.status(201).send(newTodo);
-  } catch (error) {
-    return res.status(404).send("Something went wrong");
-  }
+  const newTodo = await TODO.create(req.body);
+
+  return res.status(201).send(newTodo);
 });
 
 const getTodos = asyncWrapper(async (req, res) => {
-  try {
-    const todos = await TODO.find({});
-    if (!todos) {
-      return res.status(404).json({ msg: "Couldn't get todos" });
-    }
-    return res.status(201).json({ todos });
-  } catch (error) {
-    return res.status(404).send("Something went wrong");
+  const todos = await TODO.find({});
+  if (!todos) {
+    return res.status(404).json({ msg: "Couldn't get todos" });
   }
+
+  return res.status(201).json({ todos });
 });
 
 const getSingleTodo = asyncWrapper(async (req, res) => {
   const { id } = req.params;
-  try {
-    const todo = await TODO.findOne({ _id: id });
-    if (!todo) {
-      return res.status(404).json({ msg: `No todo found by that id: ${id}` });
-    }
-    return res.status(201).json({ todo });
-  } catch (error) {
-    return res.send("Something went wrong");
+
+  const todo = await TODO.findOne({ _id: id });
+  if (!todo) {
+    return res.status(404).json({ msg: `No todo found by that id: ${id}` });
   }
+
+  return res.status(201).json({ todo });
 });
 
 const updateTodo = asyncWrapper(async (req, res) => {
   const { id } = req.params;
 
-  try {
-    const todo = await TODO.findOneAndUpdate({ _id: id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!todo) {
-      return res.status(404).json({ msg: `No todo found by that id: ${id}` });
-    }
-    return res.json({ todo });
-  } catch (error) {
-    return res.send("Something went wrong");
+  const todo = await TODO.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!todo) {
+    return res.status(404).json({ msg: `No todo found by that id: ${id}` });
   }
+
+  return res.json({ todo });
 });
 
 const deleteTodo = asyncWrapper(async (req, res) => {
   const { id } = req.params;
-  try {
-    const todo = await TODO.findOneAndDelete({ _id: id });
-    if (!todo) {
-      return res.status(404).json({ msg: `No todo found by that id: ${id}` });
-    }
-    return res.send("Successfully deleted");
-  } catch (error) {
-    return res.send("Something went wrong");
+
+  const todo = await TODO.findOneAndDelete({ _id: id });
+  if (!todo) {
+    return res.status(404).json({ msg: `No todo found by that id: ${id}` });
   }
+
+  return res.send("Successfully deleted");
 });
 
 module.exports = {
